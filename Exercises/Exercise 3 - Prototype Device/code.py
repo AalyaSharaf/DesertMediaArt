cd8d66806cc83aa7b62313f0ad83d763cfe03d55
AalyaSharaf <70956039+AalyaSharaf@users.noreply.github.com>
2023-10-08 13:06:23 +0400

    Update code.py

--- Exercises/Exercise 3 - Prototype Device/code.py
+++ Exercises/Exercise 3 - Prototype Device/code.py
@@ -4,17 +4,16 @@ import analogio
 import digitalio
 import neopixel
 
-# Set the pin for the LDR (Light-Dependent Resistor)
+# Set the pin for the LDR
 ldr_pin = analogio.AnalogIn(board.A1)
 
 # Set the pin for the NeoPixel ring
 pixel_pin = board.D5
 num_pixels = 12
 
-# Create NeoPixel object
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2)
 
-# Define the LDR threshold
+# LDR threshold
 threshold = 40000
 
 print("Enabling NeoPixel power!")
@@ -22,14 +21,16 @@ enable = digitalio.DigitalInOut(board.D10)
 enable.direction = digitalio.Direction.OUTPUT
 enable.value = True
 
+# Creating a loop that constantly checks conditions.
 while True:
     ldr_value = ldr_pin.value
     print("LDR Value:", ldr_value)
 
+    # if LDR value is less that threshold – turn on light! Else, Light is off. 
     if ldr_value < threshold:
         pixels.fill((255, 0, 0))
     else:
         pixels.fill((0, 0, 0))
 
-    pixels.show()
-    time.sleep(0.5)  # Half-second delay for visibility and LDR reading
+    pixels.show() 
+    time.sleep(0.5)  # Half-second delay
